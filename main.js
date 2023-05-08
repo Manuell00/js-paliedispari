f32bdf671bc79bf63ea5a26bfb6721cfacce17f5
Manuell00 <manuell.caselli@gmail.com>
2023-05-08 13:35:43 +0200

    Palindrome

---
+++ main.js
@@ -0,0 +1,32 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+const parolaUtente = prompt("inserisci una parola")
+
+
+
+function palindrome(parola) {
+    // Rendo tutti i caratteri minuscoli
+    parola = parola.toLowerCase();
+
+    // Rimuovo gli spazi
+    parola = parola.replace(" ", "");
+
+    // Definisco una variabile lunghezza contente la lunghezza della parola
+    lunghezza = parola.length;
+
+    // Utilizzo un ciclo for dove verifico se la lettera partendo da sx sia uguale a quella di dx fino a metà della parola
+    for (let i = 0; i < lunghezza / 2; i++) {
+        // Se non è così restituisco false
+       if (parola[i] !== (parola[lunghezza -1 -i])) {
+        return (console.log(false))
+       }
+    }
+    // Se è verificato restituisco true
+    return (console.log(true))
+
+    
+}
+
+palindrome(parolaUtente)
